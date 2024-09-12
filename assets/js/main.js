a39b4760d204a59e744730b61ed2401260644a9c
Dharmender Kumar <dheetdev@gmail.com>
2024-09-12 17:03:06 +0530

    new assets add for css

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,36 @@ if (scrollTop) {
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+
+
+
+
+/**
+ * ACCORDION
+ */
+const collapsibleButtons = document.querySelectorAll(
+  ".collapsible-trigger-btn"
+);
+
+collapsibleButtons.forEach((collapsibleButton) => {
+  const collapsibleContentDataHeight =
+    collapsibleButton.nextElementSibling.offsetHeight;
+  collapsibleButton.nextElementSibling.style.height = 0;
+  collapsibleButton.addEventListener("click", (e) => {
+    if (
+      !e.currentTarget.parentElement.classList.contains("collapsible-tab__open")
+    ) {
+      e.currentTarget.parentElement.classList.toggle("collapsible-tab__open");
+      e.currentTarget.nextElementSibling.style.height = `${collapsibleContentDataHeight}px`;
+    } else {
+      e.currentTarget.parentElement.classList.remove("collapsible-tab__open");
+      e.currentTarget.nextElementSibling.style.height = 0;
+    }
+  });
+});
+
+
+
+
+
